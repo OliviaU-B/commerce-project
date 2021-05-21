a425c52e3e790595519536f37a97c3debd2946de
OliviaU-B <olivia.u-b@hotmail.com>
2021-05-21 17:00:35 +0100

    Add Bid and Comment models to admin site management

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import AuctionListing
+from .models import AuctionListing, Bid, Comment
 
 # Register your models here.
 
 admin.site.register(AuctionListing)
+admin.site.register(Bid)
+admin.site.register(Comment)
